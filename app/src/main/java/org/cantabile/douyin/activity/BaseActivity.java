a599d82ac1dd3a56e186984f985cc7e0e488e920
Cantabilev <1269859055@qq.com>
2017-12-11 03:03:16 +0800

    增加音乐列表 UI--> 本地音乐 最近播放 下载管理 我的电台 我的收藏

--- app/src/main/java/org/cantabile/douyin/activity/BaseActivity.java
+++ app/src/main/java/org/cantabile/douyin/activity/BaseActivity.java
@@ -10,68 +10,77 @@ import org.cantabile.douyin.interfaces.IActBase;
 
 public abstract class BaseActivity extends Activity implements IActBase {
 
-        /** 整个应用的Application **/
-        private CustomApplication app = null;
-        /** 日志输出标志 **/
-        protected final String TAG = this.getClass().getSimpleName();
+    /**
+     * 整个应用的Application
+     **/
+    private CustomApplication app = null;
+    /**
+     * 日志输出标志
+     **/
+    protected final String TAG = this.getClass().getSimpleName();
 
-        @Override
-        protected void onCreate(Bundle savedInstanceState) {
-            super.onCreate(savedInstanceState);
-            Log.d(TAG, TAG + "-->onCreate()");
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        Log.d(TAG, TAG + "-->onCreate()");
 
-            // 获取整个应用的Application
-            app = CustomApplication.getInstance();
-            initVariables();
-            initView();
-            initEvent();
-            loadData();
-            doBusiness();
-        }
-
-        @Override
-        protected void onStart() {
-            super.onStart();
-            Log.d(TAG, TAG + "-->onStart()");
-        }
-
-        @Override
-        protected void onResume() {
-            super.onResume();
-            Log.d(TAG, TAG + "-->onResume()");
-            resume();
-        }
-
-        @Override
-        protected void onPause() {
-            super.onPause();
-            Log.d(TAG, TAG + "-->onPause()");
-        }
-
-        @Override
-        protected void onRestart() {
-            super.onRestart();
-            Log.d(TAG, TAG + "-->onRestart()");
-        }
-
-        @Override
-        protected void onStop() {
-            super.onStop();
-            Log.d(TAG, TAG + "-->onStop()");
-        }
-
-        @Override
-        protected void onDestroy() {
-            super.onDestroy();
-            Log.d(TAG, TAG + "-->onDestroy()");
-            destroy();
-        }
-
-        /**
-         * 获取整个应用的Application
-         */
-        public CustomApplication getApp() {
-            return this.app;
-        }
+        // 获取整个应用的Application
+        app = CustomApplication.getInstance();
+        initVariables();
+        initView();
+        initEvent();
+        loadData();
+        doBusiness();
     }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.d(TAG, TAG + "-->onStart()");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.d(TAG, TAG + "-->onResume()");
+        resume();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.d(TAG, TAG + "-->onPause()");
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        Log.d(TAG, TAG + "-->onRestart()");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.d(TAG, TAG + "-->onStop()");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.d(TAG, TAG + "-->onDestroy()");
+        destroy();
+    }
+
+    @Override
+    public void overridePendingTransition(int enterAnim, int exitAnim) {
+        super.overridePendingTransition(enterAnim, exitAnim);
+    }
+
+    /**
+     * 获取整个应用的Application
+     */
+    public CustomApplication getApp() {
+        return this.app;
+    }
+}
 
--- app/src/main/java/org/cantabile/douyin/activity/launch/LaunchActivity.java
+++ app/src/main/java/org/cantabile/douyin/activity/launch/LaunchActivity.java
@@ -3,18 +3,27 @@ package org.cantabile.douyin.activity.launch;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
 
 import org.cantabile.douyin.R;
 import org.cantabile.douyin.activity.home.MainActivity;
 
 public class LaunchActivity extends Activity {
 
+    private Handler handler = new Handler(){
+        @Override
+        public void handleMessage(Message msg) {
+            Intent intent = new Intent(LaunchActivity.this, MainActivity.class);
+            startActivity(intent);
+            finish();
+        }
+    };
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.act_launch);
-        Intent intent = new Intent(LaunchActivity.this, MainActivity.class);
-        startActivity(intent);
-        finish();
+        handler.sendEmptyMessageDelayed(0, 2000);
     }
 }
